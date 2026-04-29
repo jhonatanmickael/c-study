6dfea382e955e609d7145668c568915e3666fbb1
Jhonatan <jhonata.mickael90@gmail.com>
2026-04-29 14:35:03 -0300

    refactor(q15): change triangle function to void procedure

--- university-projects/thyago-tenorio-list-02/15-triangle-classifier/q15.c
+++ university-projects/thyago-tenorio-list-02/15-triangle-classifier/q15.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 // Verificar se é triangulo
-int triangulo(double l1, double l2, double l3) {
-	if(l1>=l2+l3) return 0;
-	else if(l2>=l1+l3) return 0;
-	else if(l3>=l1+l2) return 0;
-	
+void triangulo(double l1, double l2, double l3) {
+    int aux;
+	if(l1>=l2+l3) aux=0;
+	else if(l2>=l1+l3) aux=0;
+	else if(l3>=l1+l2) aux=0;
 
-	// 1: Equilátero; 2: Isósceles; 3: Escaleno.
-	if(l1==l2 && l1==l3 && l2==l3) return 1;
-	
-	else if(l1==l2 || l1==l3 || l2==l3) return 2;
-	
-	else return 3;
-}
+    else {
+    	// 1: Equilátero; 2: Isósceles; 3: Escaleno.
+    	if(l1==l2 && l1==l3 && l2==l3) aux=1;
+    	else if(l1==l2 || l1==l3 || l2==l3) aux=2;
+    	else aux=3;
+    }
 
-int main() {
-	double l1, l2, l3;
-	if(scanf("%lf %lf %lf", &l1, &l2, &l3)!=3) return 1;
-	
-	switch(triangulo(l1, l2, l3)) {
-		case 1: 
+    switch(aux) {
+		case 1:
 			printf("Triângulo Equilátero.\n");
 			break;
 		case 2:
@@ -29,9 +24,17 @@ int main() {
 		case 3:
 			printf("Triângulo Escaleno.\n");
 			break;
-		default: 
+		default:
 			printf("Não é um triângulo\n");
-	}		
 
+    }
+}
+
+int main() {
+	double l1, l2, l3;
+	if(scanf("%lf %lf %lf", &l1, &l2, &l3)!=3) return 1;
+    triangulo(l1, l2, l3);
 	return 0;
 }
+
+
